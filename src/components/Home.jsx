aba6419b88e2b5e80c5c9dd53e990cef95078521
AInesC <87500126+AInesC@users.noreply.github.com>
2022-04-18 21:33:33 +0100

    Adicionados dados do state na home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,14 @@
 export default function Home({ stocks }) {
 	console.log(stocks);
 	return (
-		<div>
-			<h2></h2>
-			<p></p>
-		</div>
+		<main className="Home">
+			{stocks.map((stock, index) => (
+				<section key="index">
+					<h2>{stock.name}</h2>
+					<h3>{stock.code}</h3>
+					<div>Preço: {stock.price}€</div>
+				</section>
+			))}
+		</main>
 	);
 }
