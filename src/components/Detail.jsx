b854bcfa9adc55f899358f6c40803bfe306b8730
AInesC <87500126+AInesC@users.noreply.github.com>
2022-04-20 19:39:24 +0100

    Adicionado botão de marcar/remover favorito na página de detalhe

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -10,7 +10,14 @@ export default function Detail() {
 		fetch("https://justivo.com/stockws.php?get&code=" + params.code)
 			.then((response) => response.json())
 			.then((dataRecieved) => setStock(dataRecieved));
-	}, []);
+	}, [params]);
+
+	function toggleFavorite() {
+		setStock((prevState) => ({
+			...prevState,
+			isFavorite: !prevState.isFavorite,
+		}));
+	}
 
 	return (
 		<>
@@ -19,6 +26,11 @@ export default function Detail() {
 					<h2>{stock.name}</h2>
 					<h3>{stock.code}</h3>
 					<div>Preço: {stock.price}€</div>
+					<div>
+						<button onClick={toggleFavorite}>
+							{stock.isFavorite === true ? "Remover " : "Marcar "} Favorito
+						</button>
+					</div>
 					<div>
 						<NavLink to="/">Voltar</NavLink>
 					</div>
